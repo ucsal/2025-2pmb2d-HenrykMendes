91990572f97bdb827dc517d02b13f816138437e6
J. Henrique Mendes <h06.987654321@gmail.com>
2025-09-04 00:06:42 -0300

    Refactor: DRY_RUN=true SMTP_AVAILABLE configurado

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,20 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+	private final MailSender MS;
+	
+    public EmailNotifier() {
+        if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+            this.MS = new TestMailSender();
+        } else {
+            this.MS = new SmtpClient();
+        }
+    }
 
 	public void welcome(User user) {
 		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
 			// Estado inicial: ainda assim usa SMTP real (bug proposital)
 		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
+		MS.send(user.email(), "Bem-vindo", "Olá " + user.name());
 	}
 }
--- src/main/java/br/com/mariojp/solid/dip/SmtpClient.java
+++ src/main/java/br/com/mariojp/solid/dip/SmtpClient.java
@@ -1,11 +1,13 @@
 package br.com.mariojp.solid.dip;
 
-public class SmtpClient {
+public class SmtpClient implements MailSender {
     public void send(String to, String subject, String body){
         if(!"true".equalsIgnoreCase(System.getProperty("SMTP_AVAILABLE"))) {
             throw new IllegalStateException("SMTP indisponível");
         }
         if(to == null || !to.contains("@")) throw new IllegalArgumentException("email inválido");
         // envia ...
+        System.out.println("[SMTP] Email enviado para " + to);
+
     }
 }
